507ce578d5ca545931e6565e85c99986d7efe906
Farras Alam Majid <38182609+Farras008@users.noreply.github.com>
2022-06-02 13:43:22 +0700

    Add files via upload

---
+++ Responsi-Prak-PBO-main/Responsi-Prak-PBO-main/ResponsiPraktikumPBO/src/responsipraktikumpbo/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package responsipraktikumpbo;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ViewMovie view = new ViewMovie();
+        ModMovie model = new ModMovie();
+        Cont controller = new Cont(model, view);
+    }
+    
+}
